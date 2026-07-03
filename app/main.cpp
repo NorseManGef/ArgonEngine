5b6a0050b6b5c9cf1dc03c869bba62d49bea654f
Garrett R. <norsemangef@protonmail.com>
2026-07-03 15:18:22 -0600

    reformat prints in argoninit to use Argon::Log

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,13 @@
 #include "ArgonAppFlowControl.h"
 #include <ArgonEngine/ArgonInit.h>
 #include <SDL3/SDL.h>
+#include <ArgonEngine/Log.h>
 ArgonAppFlowControl* controller;
 void manual_redraw(){
     if(controller->draw())Argon::swap_buffers();
 }
 int main(int argc, char** argv){
+    Argon::Log::file_init(Argon::Log::Severity::verbose, "logs/log.txt");
     Argon::initialize_engine("ArgonSoftware","ArgonApp");
     int run =1;
     Argon::set_manual_redraw(manual_redraw);
